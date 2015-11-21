51ca83a09ddec6f6687088c05795290b1e432c88
Tomas van der Wansem <tomas.vanderwansem@uniserv.com>
2015-11-21 22:48:05 +0100

    Fix spacing

--- src/measurements.c
+++ src/measurements.c
@@ -1,8 +1,8 @@
 /*
-* measurements.c
-*
-* Maintains the set of current measurements
-*/
+ * measurements.c
+ *
+ * Maintains the set of current measurements
+ */
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -23,54 +23,54 @@ static ezxml_t store;
 // or inits a new measurement store
 static void init()
 {
-   // already initialized
-   if (store != NULL)
-   return;
+    // already initialized
+    if (store != NULL)
+        return;
 
-   FILE *f = fopen(DATABASE, "r");
-   if (f != NULL)
-   {
-      // load previous version
-      store = ezxml_parse_fp(f);
-      if (store == NULL)
-      perror(ezxml_error(store));
-      else
-      return;
-   }
+    FILE *f = fopen(DATABASE, "r");
+    if (f != NULL)
+    {
+        // load previous version
+        store = ezxml_parse_fp(f);
+        if (store == NULL)
+            perror(ezxml_error(store));
+        else
+            return;
+    }
 
-   store = ezxml_new("status");
+    store = ezxml_new("status");
 }
 
 
 // Upserts a measurement value
 void measurement_set(const char *key, const char *value)
 {
-   init();
+    init();
 
-   // find existing child
-   ezxml_t child = ezxml_child(store, key);
+    // find existing child
+    ezxml_t child = ezxml_child(store, key);
 
-   if (child == NULL)
-   {
-      // add new key
-      child = ezxml_add_child(store, key, 0);
-   }
+    if (child == NULL)
+    {
+        // add new key
+        child = ezxml_add_child(store, key, 0);
+    }
 
-   ezxml_set_txt(child, value);
+    ezxml_set_txt(child, value);
 }
 
 // returns the current value of the given key
 // or an empty string if the key was not found
 const char * measurement_get(const char *key)
 {
-   init();
+    init();
 
-   ezxml_t child = ezxml_child(store, key);
+    ezxml_t child = ezxml_child(store, key);
 
-   if (child == NULL)
-   return "";
-   else
-   return ezxml_txt(child);
+    if (child == NULL)
+        return "";
+    else
+        return ezxml_txt(child);
 
 }
 
@@ -78,28 +78,28 @@ const char * measurement_get(const char *key)
 // Stores the current measurement state to disk
 void measurement_write()
 {
-   // try writing to new file
-   FILE *fnew = fopen(DATABASE ".new", "w");
-   if (!fnew)
-   {
-      perror("Can't write database");
-      return;
-   }
+    // try writing to new file
+    FILE *fnew = fopen(DATABASE ".new", "w");
+    if (!fnew)
+    {
+        perror("Can't write database");
+        return;
+    }
 
-   // convert to string & write
-   char * xml = ezxml_toxml(store);
-   if (fputs(xml, fnew) == EOF)
-   {
-      free(xml);
-      return;
-   }
+    // convert to string & write
+    char * xml = ezxml_toxml(store);
+    if (fputs(xml, fnew) == EOF)
+    {
+        free(xml);
+        return;
+    }
 
-   free(xml);
-   fclose(fnew);
+    free(xml);
+    fclose(fnew);
 
-   // we use rename-overwrite to ensure
-   // atomic write
-   rename(DATABASE ".new", DATABASE);
+    // we use rename-overwrite to ensure
+    // atomic write
+    rename(DATABASE ".new", DATABASE);
 }
 
 
@@ -107,68 +107,68 @@ void measurement_write()
 
 static void test_getset()
 {
-   // fresh start
-   unlink(DATABASE);
-   store = NULL;
+    // fresh start
+    unlink(DATABASE);
+    store = NULL;
 
-   measurement_set("wok", "bar");
-   measurement_set("foo", "3.0");
-   measurement_set("foo", "2.0");
+    measurement_set("wok", "bar");
+    measurement_set("foo", "3.0");
+    measurement_set("foo", "2.0");
 
-   assert(strcmp(measurement_get("wok"), "bar") == 0);
-   assert(strcmp(measurement_get("foo"), "2.0") == 0);
+    assert(strcmp(measurement_get("wok"), "bar") == 0);
+    assert(strcmp(measurement_get("foo"), "2.0") == 0);
 
-   puts("Measurement tests OK");
+    puts("Measurement tests OK");
 }
 
 static void test_persist()
 {
-   // fresh start
-   unlink(DATABASE);
-   store = NULL;
+    // fresh start
+    unlink(DATABASE);
+    store = NULL;
 
-   measurement_set("wok", "baz");
-   measurement_write();
-   measurement_write();
+    measurement_set("wok", "baz");
+    measurement_write();
+    measurement_write();
 
-   // force reload
-   store = NULL;
+    // force reload
+    store = NULL;
 
 
-   // should get data from disk
-   assert(strcmp(measurement_get("wok"), "baz") == 0);
+    // should get data from disk
+    assert(strcmp(measurement_get("wok"), "baz") == 0);
 }
 
 static void test_failpersist()
 {
-   // fresh start
-   unlink(DATABASE);
-   store = NULL;
+    // fresh start
+    unlink(DATABASE);
+    store = NULL;
 
-   measurement_set("wok", "baz");
-   measurement_write();
+    measurement_set("wok", "baz");
+    measurement_write();
 
-   // mess up template file
-   FILE *f = fopen(DATABASE, "w");
-   fputs("ugh", f);
-   fclose(f);
+    // mess up template file
+    FILE *f = fopen(DATABASE, "w");
+    fputs("ugh", f);
+    fclose(f);
 
-   // force reload
-   store = NULL;
+    // force reload
+    store = NULL;
 
-   // should be empty
-   assert(strcmp(measurement_get("wok"), "") == 0);
+    // should be empty
+    assert(strcmp(measurement_get("wok"), "") == 0);
 }
 
 
 int main(void) {
 
-   test_getset();
-   test_failpersist();
-   test_persist();
+    test_getset();
+    test_failpersist();
+    test_persist();
 
 
-   return 0;
+    return 0;
 }
 
 #endif
--- src/request.c
+++ src/request.c
@@ -1,8 +1,8 @@
 /*
-* request.c
-*
-* parses and processes individual requests
-*/
+ * request.c
+ *
+ * parses and processes individual requests
+ */
 
 #include <assert.h>
 #include <string.h> 
@@ -18,15 +18,16 @@
 // handles parsed update request
 void process_update(ezxml_t request)
 {
-   // set each child in the measurement store
-   ezxml_t child = request->child;
-   while(child != NULL)
-   {
-      // key = tagname, value=text content
-      measurement_set(child->name, ezxml_txt(child));
+    // set each child in the measurement store
+    ezxml_t child = request->child;
+    while(child != NULL)
+    {
+        // key = tagname, value=text content
+        measurement_set(child->name, ezxml_txt(child));
 
-      child = child->sibling;
-   }
+        child = child->sibling;
+    }
+    measurement_write();
 }
 
 // Finds the value for the given key and appends 
@@ -34,24 +35,24 @@ void process_update(ezxml_t request)
 // *result is realloced and *alloced is updated
 static void append_result(const char *key, char **result, int *alloced)
 {
-   const char *val = measurement_get(key);
+    const char *val = measurement_get(key);
 
-   // construct xml for this key/val
-   ezxml_t val_xml = ezxml_new(key);
-   ezxml_set_txt(val_xml, val);
-   char *t = ezxml_toxml(val_xml);
+    // construct xml for this key/val
+    ezxml_t val_xml = ezxml_new(key);
+    ezxml_set_txt(val_xml, val);
+    char *t = ezxml_toxml(val_xml);
 
-   // make space in result
-   *alloced += strlen(t) + 3; // include WS
-   *result = realloc(*result, *alloced);
+    // make space in result
+    *alloced += strlen(t) + 3; // include WS
+    *result = realloc(*result, *alloced);
 
-   // add line to result
-   strcat(*result, "  ");
-   strcat(*result, t);
-   strcat(*result, "\n");
+    // add line to result
+    strcat(*result, "  ");
+    strcat(*result, t);
+    strcat(*result, "\n");
 
-   free(t);
-   ezxml_free(val_xml);
+    free(t);
+    ezxml_free(val_xml);
 }
 
 
@@ -59,21 +60,21 @@ static void append_result(const char *key, char **result, int *alloced)
 // returns xml doc that must be freed
 char * process_retrieve(ezxml_t request)
 {
-   // initialize result-doc
-   int alloced = strlen("<status>\n</status>\n")+1;
-   char *result = malloc(alloced);
-   strcpy(result, "<status>\n");
+    // initialize result-doc
+    int alloced = strlen("<status>\n</status>\n")+1;
+    char *result = malloc(alloced);
+    strcpy(result, "<status>\n");
 
-   // browse requested keys
-   ezxml_t child = ezxml_child(request, "key");
-   while(child != NULL)
-   {
-      append_result(ezxml_txt(child), &result, &alloced);
+    // browse requested keys
+    ezxml_t child = ezxml_child(request, "key");
+    while(child != NULL)
+    {
+        append_result(ezxml_txt(child), &result, &alloced);
 
-      child = ezxml_next(child);
-   }
-   strcat(result, "</status>\n");
-   return result;
+        child = ezxml_next(child);
+    }
+    strcat(result, "</status>\n");
+    return result;
 }
 
 
@@ -84,35 +85,35 @@ char * process_retrieve(ezxml_t request)
 char *request_process(char *request, int len)
 {
 
-   // parse xml-request
-   ezxml_t rq = ezxml_parse_str(request, len);
-   if (rq == NULL || rq->name == NULL)
-   {
-      perror(ezxml_error(rq));
-      return NULL;
-   }
+    // parse xml-request
+    ezxml_t rq = ezxml_parse_str(request, len);
+    if (rq == NULL || rq->name == NULL)
+    {
+        perror(ezxml_error(rq));
+        return NULL;
+    }
 
-   // delegate
-   if (strcmp(rq->name, "update") == 0)
-   {
-      process_update(rq);
+    // delegate
+    if (strcmp(rq->name, "update") == 0)
+    {
+        process_update(rq);
 
-      ezxml_free(rq);
-      return strdup("");
-   } 
-   else if (strcmp(rq->name, "retrieve") == 0)
-   {
-      char *resp = process_retrieve(rq);
+        ezxml_free(rq);
+        return strdup("");
+    } 
+    else if (strcmp(rq->name, "retrieve") == 0)
+    {
+        char *resp = process_retrieve(rq);
 
-      ezxml_free(rq);
-      return resp;
-   }
-   else
-   {
-      // unknown tag
-      ezxml_free(rq);
-      return NULL;
-   }
+        ezxml_free(rq);
+        return resp;
+    }
+    else
+    {
+        // unknown tag
+        ezxml_free(rq);
+        return NULL;
+    }
 
 }
 
@@ -122,57 +123,57 @@ char *request_process(char *request, int len)
 // searches for specific closing tags
 int request_findend(const char *input)
 {
-   int found_end = -1, i, n;
+    int found_end = -1, i, n;
 
-   // We search for a (self) closing tag named one of these
-   static const char *end_markers[] = { "update", "retrieve" };
+    // We search for a (self) closing tag named one of these
+    static const char *end_markers[] = { "update", "retrieve" };
 
-   for(i = 0; i < sizeof(end_markers)/sizeof(char*); i ++)
-   {
-      int len = strlen(end_markers[i]);
-      char *p = (char *)input;
+    for(i = 0; i < sizeof(end_markers)/sizeof(char*); i ++)
+    {
+        int len = strlen(end_markers[i]);
+        char *p = (char *)input;
 
-      // loop though matches of this tag
-      for(;;) 
-      {
-         p = strstr(p+1, end_markers[i]);
-         if (p == NULL)
-            break;
+        // loop though matches of this tag
+        for(;;) 
+        {
+            p = strstr(p+1, end_markers[i]);
+            if (p == NULL)
+                break;
 
-         n = (int)(p - input); // found position
+            n = (int)(p - input); // found position
 
-         if (n < 1 || (n > found_end && found_end!=-1))
-            continue; // we found an earlier end
+            if (n < 1 || (n > found_end && found_end!=-1))
+                continue; // we found an earlier end
 
-         // this is an end-marker if 
-         // 1. preceded by </ end succeeded by WS + >
-         if (n >= 2 && input[n -2] == '<' && input[n-1] == '/')
-         {
-            int q = n + len;
-            while (isspace(input[q]))
-               q++;
-            if (input[q] == '>')
+            // this is an end-marker if 
+            // 1. preceded by </ end succeeded by WS + >
+            if (n >= 2 && input[n -2] == '<' && input[n-1] == '/')
             {
-               found_end = q+1;
-               break;
+                int q = n + len;
+                while (isspace(input[q]))
+                    q++;
+                if (input[q] == '>')
+                {
+                    found_end = q+1;
+                    break;
+                }
             }
-         }
-         // 2. preceded by < end succeeded by WS + /> 
-         else if (input[n-1] == '<')
-         {
-            int q = n + len;
-            while(isspace(input[q]))
-               q++;
-            if (input[q] == '/' && input[q+1] == '>')
+            // 2. preceded by < end succeeded by WS + /> 
+            else if (input[n-1] == '<')
             {
-               found_end = q + 2;
-               break;
+                int q = n + len;
+                while(isspace(input[q]))
+                    q++;
+                if (input[q] == '/' && input[q+1] == '>')
+                {
+                    found_end = q + 2;
+                    break;
+                }
             }
-         }
-      }
-   }
+        }
+    }
 
-   return found_end;
+    return found_end;
 }
 
 
@@ -182,98 +183,98 @@ int request_findend(const char *input)
 static int count_set = 0;
 void measurement_set(const char *key, const char *value)
 {
-   count_set++;
+    count_set++;
 }
 
 const char * measurement_get(const char *key)
 {
-   return "wok";
+    return "wok";
 }
 
 void test_findend()
 {
-   // findend should take one request
-   assert( 
-      request_findend("<update>x</update><update></update>") 
-      == strlen("<update>x</update>")
-   );
+    // findend should take one request
+    assert( 
+            request_findend("<update>x</update><update></update>") 
+            == strlen("<update>x</update>")
+          );
 
-   // handle self-closing tags
-   assert( request_findend("<update />") == strlen("<update />"));
+    // handle self-closing tags
+    assert( request_findend("<update />") == strlen("<update />"));
 
-   // without space
-   assert( request_findend("<retrieve/>") == strlen("<retrieve/>"));
+    // without space
+    assert( request_findend("<retrieve/>") == strlen("<retrieve/>"));
 
-   // partial request
-   assert( request_findend("<update><mykey></mykey>") == -1); 
+    // partial request
+    assert( request_findend("<update><mykey></mykey>") == -1); 
 }
 
 // test with const char's
 char * dotest_process(const char *p)
 {
-   char *m = strdup(p);
-   char *res = request_process(m, strlen(m));
-   free(m);
-   return res;
+    char *m = strdup(p);
+    char *res = request_process(m, strlen(m));
+    free(m);
+    return res;
 }
 
 void test_update()
 {
-   count_set = 0;
+    count_set = 0;
 
-   char *res = dotest_process(
-      "<update>"
-      "<mykey>myval</mykey>"
-      "<myk1>1</myk1>"
-      "</update>"
-   );
-   assert(*res == 0); // no response
-   assert(count_set == 2); // 2 updates
-   free(res);
+    char *res = dotest_process(
+            "<update>"
+            "<mykey>myval</mykey>"
+            "<myk1>1</myk1>"
+            "</update>"
+            );
+    assert(*res == 0); // no response
+    assert(count_set == 2); // 2 updates
+    free(res);
 
-   // test invalid
-   res = dotest_process(
-      "<wok>"
-      "<mykey>myval</mykey>"
-      "<myk1>1</myk1>"
-      "</wok>"
-   );
-   assert(res == NULL); // no response
-   assert(count_set == 2); // still 2 updates
+    // test invalid
+    res = dotest_process(
+            "<wok>"
+            "<mykey>myval</mykey>"
+            "<myk1>1</myk1>"
+            "</wok>"
+            );
+    assert(res == NULL); // no response
+    assert(count_set == 2); // still 2 updates
 
-   // test empty update
-   res = dotest_process( "<update />");
+    // test empty update
+    res = dotest_process( "<update />");
 
-   assert(*res == '\0'); // no response
-   assert(count_set == 2); // still 2 updates
+    assert(*res == '\0'); // no response
+    assert(count_set == 2); // still 2 updates
 }
 
 void test_receive()
 {
-   char *res = dotest_process(
-      "<retrieve>"
-      "  <key>key1</key>"
-      "  <key>key2</key>"
-      "</retrieve>"
-   );
-   
-   // should be nicely aligned
-   assert(strcmp(res, 
-      "<status>\n"
-      "  <key1>wok</key1>\n"
-      "  <key2>wok</key2>\n"
-      "</status>\n") == 0
-   );
+    char *res = dotest_process(
+            "<retrieve>"
+            "  <key>key1</key>"
+            "  <key>key2</key>"
+            "</retrieve>"
+            );
+
+    // should be nicely aligned
+    assert(strcmp(res, 
+                "<status>\n"
+                "  <key1>wok</key1>\n"
+                "  <key2>wok</key2>\n"
+                "</status>\n") == 0
+          );
 
 }
 
 int main(void) {
 
-   test_findend();
-   test_update();
-   test_receive();
-   puts("Request tests OK");
-   return 0;
+    test_findend();
+    test_update();
+    test_receive();
+    puts("Request tests OK");
+    return 0;
 }
 
 #endif
